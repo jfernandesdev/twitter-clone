67bdfa23698464ea8a685f0c19f92e7d2f1f804e
jfernandesdev <jfernandes.dev@gmail.com>
2021-10-06 20:48:00 -0300

    layout component creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import GlobalStyles from './styles/GlobalStyles';
-
+import Layout from './components/Layout';
 function App() {
   return (
     <>
     <GlobalStyles />
+    <Layout />
     <h1>Hello World!</h1>
     </>
   );
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Main: React.FC = () => {
+  return (
+    <h1>Eu sou o feed!</h1>
+  );
+}
+
+export default Main;
